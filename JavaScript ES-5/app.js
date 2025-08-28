b79d92c6d15533f02624cf14b4e7f52ffd086a01
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-08-28 21:27:27 +0500

    Add files via upload

---
+++ JavaScript ES-5/app.js
@@ -0,0 +1,62 @@
+// alert("Hello World");
+// document.write("Hello World");
+// console.log("Hello World");
+
+// var a = 5;
+// var b = 10;
+// var c = a + b;
+// alert(c);
+
+// var x = "ali";
+// var y = 15;
+// var z = x+y;
+// alert(z);
+
+// var f = "farooq";
+// var f = "faraz";
+// alert(f);
+
+// var a= "ahmed";
+// var b = 5;
+// var c = a*b;
+// alert(c)
+
+// var x = 5;
+// var y = "3"
+// var z = x+y;
+// alert(z)
+
+// var f = "4";
+// var g = 2;
+// var h = f/g;
+// alert(h)
+
+// var a;
+// a = 5;
+// alert(a)
+
+// var x = 10;
+// var y = x%3;
+// alert(y)
+
+
+// var f = (6+8)/4+5*(3-6)
+// alert(f)
+
+// Increment:
+// Post Increment : x++ x=x+1
+// Pre Increment : ++x
+// Decrement:
+// Post Decrement : x-- x=x-1
+// Pre Decrement : --x
+
+var x = 3;
+var y =x++ + x + ++x + x + x++ + ++x;
+        // 3 + 4 + 5 + 5 + 5 + 7
+alert(y)
+
+
+var a = 2;
+var b = a - ++a + a++ - ++a - a + ++a + a - a + a++;
+        // 2 - 3 + 3 - 5 - 5 + 6 + 6 - 6 + 6
+alert(b)
