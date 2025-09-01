ba2cce16907684689a25f1dcce16f6d8cc81a3f2
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-09-01 21:35:27 +0500

    Add files via upload

---
+++ JavaScript/class 02/app.js
@@ -0,0 +1,175 @@
+// web and mobile
+
+// js engine
+
+// google chrome v8 engine
+
+// mozilla firefox spidermonkey engine
+
+// concept of programming 60 - 70 % same
+
+// if else
+// array
+// object
+// function
+// loop
+
+// alert()
+// document.write()
+// console.log()
+
+// abc(); js user defined function
+
+// alert(); js builtin function
+
+// document.write() js builtin method
+
+// console.log() js builtin method
+
+// builtin
+
+// user defined
+
+// runtime error
+
+// compile time error
+
+// result
+
+// alert("faraz");
+
+// document.write("hamza");
+
+// console.log("hello students");
+
+// console.log(50);
+
+// variables
+
+// var names = "faraz";
+
+// console.log(names);
+
+// var number = 12;
+
+// console.log(number);
+
+// variable declaration
+
+// var number ;
+
+// console.log(number);
+
+// variable declaration & intialization
+
+// var number1 = 12;
+
+// console.log(number1);
+
+// data types
+
+// string "faraz","ali","hamza","farooq"
+
+// number 1,2,3,4,5,6,7,8,9,10
+
+// var number;
+
+// number = 50;
+
+// console.log(number);
+
+// var name = "Faraz";
+
+// console.log(name);
+
+// var firstName = "MUHAMMAD";
+
+// var lastName = "FARAZ";
+
+// console.log(firstName + " " + lastName);
+
+// camel case ===> fullName, lastName, firstName;
+
+// snake case ===> full_name, last_name;
+
+// Pascal Case ===> LastName,FullName
+
+// console.log();
+
+// var firstName = "MUHAMMAD";
+
+// var lastName = "FARAZ";
+
+// console.log("hello", firstName, lastName, "you are selected for this position");
+
+// console.log("hello Muhammad faraz, you are selected for this position");
+
+// + concact, add
+
+// var num1 = "faraz";
+
+// var num2;
+
+// console.log(num1 - num2);
+
+// var num1 = 5;
+// var num2 = 10;
+// var name = "ali";
+
+// console.log(num1 + name + num2);
+
+// var num = "10";
+
+// console.log(typeof num);
+
+// var num = 5;
+
+// num = num + 1;
+
+// console.log(num);
+
+// post increment , pre increment
+
+// post decrement , pre decrement
+
+// var number = 2;
+
+// console.log("before increment ", number);
+
+// ++number;
+
+// console.log("afterd increment ", number);
+
+// var num = 5;
+
+// var exp = num++ - --num + num-- + num;
+
+// console.log(5 - 5 + 5 + 4);
+
+// var num1 = +prompt("Enter your first number");
+
+// var num2 = +prompt("Enter your second number");
+
+// console.log(num1 * num2);
+
+// var firstname = prompt("Enter your first name");
+
+// var lastname = prompt("Enter your last name");
+
+// console.log(firstname, lastname);
+
+// Alert Chapter 1
+
+// i)
+
+// alert("welcome to our website");
+
+// ii)
+
+// alert("Error! please enter a valid password");
+
+// iii)
+
+// alert("Welcome to JS Land... \n Happy Coding");
+
+// vi and vii done
