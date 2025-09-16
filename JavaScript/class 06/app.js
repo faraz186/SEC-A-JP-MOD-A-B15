2cbff345c34f043425e056e80384607f7569c78b
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-09-16 19:48:31 +0500

    Update app.js

--- JavaScript/class 06/app.js
+++ JavaScript/class 06/app.js
@@ -1,5 +1,5 @@
 var userInput = prompt("Enter a string");
-     
+      
 var len = userInput.length;
 
 var flag = false;
@@ -114,6 +114,7 @@ var daysArr = [
 var date = new Date();
 
 console.log(daysArr[date.getDay()]);
+
 
 
 
