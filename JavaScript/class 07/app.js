8d57ccabfc68f941727c026c8bee599c81440339
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-09-19 00:11:39 +0500

    Update app.js

--- JavaScript/class 07/app.js
+++ JavaScript/class 07/app.js
@@ -1,5 +1,5 @@
 var randomValues =
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";    
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";     
 
 var randomValLen = randomValues.length;
 
@@ -132,5 +132,6 @@ function add() {
 }
 
 add();
+
 
 
