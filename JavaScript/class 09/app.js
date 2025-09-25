151d4fcd4a92da48b35ec069399fb49f05e6ed62
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-09-25 21:24:19 +0500

    calculator app and chapter 52 complete...

---
+++ JavaScript/class 09/app.js
@@ -0,0 +1,58 @@
+// alert("welcome user");
+
+// function greet() {
+//   console.log("welcome to our website please login..");
+// }
+
+// function changingStyle(element) {
+//   element.style.color = "red";
+//   element.style.textDecoration = "underline";
+//   element.style.margin = "10px";
+//   element.style.padding = "5px";
+//   element.style.border = "5px solid green";
+// }
+
+// function submit() {
+//   var inputEmail = document.getElementById("email");
+
+//   var inputPassword = document.getElementById("password");
+
+//   if (inputEmail.value == "" || inputPassword.value == "") {
+//     Swal.fire({
+//       title: "Failed!",
+//       text: "Required fields are missing!",
+//       icon: "error",
+//     });
+//   } else {
+//     Swal.fire({
+//       title: "Good job!",
+//       text: "Form submitted!",
+//       icon: "success",
+//     });
+//     console.log(inputEmail.value);
+//     console.log(inputPassword.value);
+//   }
+// }
+
+// function set() {
+//   var input = document.getElementById("name");
+//   console.log(input);
+
+//   input.value = "faraz";
+// }
+
+// var h1Heading = document.getElementById("heading");
+// var pElement = document.getElementById("para");
+
+// console.log(h1Heading.innerText);
+
+// console.log(pElement.innerText);
+
+
+
+
+// var linkElement = document.getElementById("anchor");
+
+// linkElement.innerText = "link";
+
+// console.log(linkElement);
