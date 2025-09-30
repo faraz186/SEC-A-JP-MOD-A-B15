fbe0b5357feb6fe19f181fe30bf7344c1a9f2e3e
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-09-30 20:47:58 +0500

    Update app.js

--- JavaScript/class 10/app.js
+++ JavaScript/class 10/app.js
@@ -1,5 +1,5 @@
 var h1Element = document.getElementById("h1");
- 
+  
 var pELement = document.getElementById("p1");
 
 console.log(h1Element.innerText);
@@ -121,3 +121,4 @@ function greet() {
 }
 
 setTimeout(greet, 8000);
+
