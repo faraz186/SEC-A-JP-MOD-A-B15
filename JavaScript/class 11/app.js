e21f96111ccd462238ca1ffc1b44b917a140becd
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-10-02 20:49:19 +0500

    Add files via upload

---
+++ JavaScript/class 11/app.js
@@ -0,0 +1,16 @@
+// document.getElementById()
+// document.getElementsByClassName()
+// document.getElementsByTagName()
+
+
+
+var pElement = document.childNodes[0].childNodes[2].childNodes[1].childNodes[3];
+
+console.log(pElement);
+
+
+var pElement = document.childNodes[0].childNodes[1].childNodes;
+
+// text = junk artifact / garbage value
+
+console.log(pElement);
