dfb8c314b6838ea41c02ef5a7b721c7a50f9d845
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-10-13 21:19:11 +0500

    Add files via upload

---
+++ JavaScript/class 14/app.js
@@ -0,0 +1,34 @@
+// function greetWorld() {
+//   try {
+//     var greeting = "Hello world!";
+//     alert(greeting);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// greetWorld();
+
+// console.log(windo);
+
+// var arrayOfObject = [
+//   {
+//     id: 1,
+//     name: "abc",
+//     city: "karachi",
+//   },
+//   {
+//     id: 2,
+//     name: "cde",
+//     city: "lahore",
+//   },
+//   {
+//     id: 3,
+//     name: "xyz",
+//     city: "multan",
+//   },
+// ];
+
+// for (var i = 0; i < arrayOfObject.length; i++) {
+//   console.log(arrayOfObject[i]);
+// }
